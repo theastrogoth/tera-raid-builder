037140401d4467464f23615a66a504405aa0ec48
theAstroGoth <theastrogoth@gmail.com>
2023-07-31 18:42:57 -0500

    static HTML export test

--- src/uicomponents/GraphicsButton.tsx
+++ src/uicomponents/GraphicsButton.tsx
@@ -7,14 +7,39 @@ import Box from '@mui/material/Box';
 import { Pokemon, Generations, Field } from "../calc";
 import { MoveName, TypeName } from "../calc/data/interface";
 import { getItemSpriteURL, getPokemonArtURL, getTypeIconURL, getTeraTypeIconURL } from "../utils";
-import { RaidBattleInfo, Raider, RaidState, BuildInfo, RaidTurnInfo, RaidInputProps } from "../raidcalc/interface";
+import { Raider, RaidState, RaidTurnInfo } from "../raidcalc/interface";
+import { RaidInputProps } from "../raidcalc/inputs";
 import { LightBuildInfo, LightPokemon, LightTurnInfo } from "../raidcalc/hashData";
+
+import PokemonSummary from "./PokemonSummary";
+import BossSummary from "./BossSummary";
 
 import html2canvas from 'html2canvas';
 import { saveAs } from 'file-saver';
 
 import Button from "@mui/material/Button"
 import { Hidden } from "@mui/material";
+
+import ReactDomServer from "react-dom/server";
+
+function saveStaticHTML(raidInputProps: RaidInputProps, title?: string, notes?: string, credits?: string) {
+    const htmlContent = [ ReactDomServer.renderToStaticMarkup(
+        // This would need to be filled out with everything we want to show. I don't think we would reuse many of the UI components here.
+        <>
+            <PokemonSummary pokemon={raidInputProps.pokemon[1]} setPokemon={(p: Raider) => {return; }} prettyMode={true} />
+        </>
+    )];
+    console.log(htmlContent)
+    var bl = new Blob(htmlContent, {type: "text/html"});
+    var a = document.createElement("a");
+    a.href = URL.createObjectURL(bl);
+    a.download = (title || "strategy" ) + ".html";
+    a.hidden = true;
+    document.body.appendChild(a);
+    a.innerHTML = "something random - nobody will see this, it doesn't matter what you put here";
+    a.click();
+    
+}
 
 
 function GraphicsButton({title, notes, credits, raidInputProps, setTitle, setNotes, setCredits, setPrettyMode}: 
@@ -28,15 +53,16 @@ function GraphicsButton({title, notes, credits, raidInputProps, setTitle, setNot
 
     const handleDownload = () => {
         try {
-            html2canvas(document.body, {allowTaint: true, useCORS: true}).then((canvas) => {
-                canvas.toBlob((blob) => {
-                    if (blob) {
-                        saveAs(blob, title + '.png');
-                    } else {
-                        saveAs(getPokemonArtURL("wo-chien"), "live_reaction.png");
-                    }
-                });
-            });
+            // html2canvas(document.body, {allowTaint: true, useCORS: true}).then((canvas) => {
+            //     canvas.toBlob((blob) => {
+            //         if (blob) {
+            //             saveAs(blob, title + '.png');
+            //         } else {
+            //             saveAs(getPokemonArtURL("wo-chien"), "live_reaction.png");
+            //         }
+            //     });
+            // });
+            saveStaticHTML(raidInputProps, title, notes, credits);
         } catch (e) {
             console.log(e)
         }

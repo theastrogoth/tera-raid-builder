64f270eb50ab0ac1ac89c385736211c6e5b47437
theAstroGoth <theastrogoth@gmail.com>
2023-09-29 17:27:06 -0500

    add turn slider to HP display

--- src/uicomponents/RaidControls.tsx
+++ src/uicomponents/RaidControls.tsx
@@ -12,29 +12,29 @@ import MoveDisplay from './MoveDisplay';
 import { RaidInputProps } from "../raidcalc/inputs";
 import { RaidBattleResults } from "../raidcalc/RaidBattle";
 import { Pokemon } from '../calc';
-import { Typography } from '@mui/material';
+import { Slider, Typography } from '@mui/material';
 
 
 const raidcalcWorker = new Worker(new URL("../workers/raidcalc.worker.ts", import.meta.url));
 
 const HpBar = styled(LinearProgress)(({ theme }) => ({
-    height: 10,
-    borderRadius: 5,
+    height: 8,
+    borderRadius: 4,
     [`&.${linearProgressClasses.colorPrimary}`]: {
       backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
     },
     [`& .${linearProgressClasses.bar}`]: {
-      borderRadius: 5,
+      borderRadius: 4,
       backgroundColor: "#30B72D",
     },
 }));
 
-function HpDisplayLine({role, curhp, maxhp}: {role: string, curhp: number, maxhp: number}) {
+function HpDisplayLine({role, curhp, maxhp, kos}: {role: string, curhp: number, maxhp: number, kos: number}) {
     const hpPercent = curhp / maxhp * 100;
     const color = (hpPercent > 50 ? "#30B72D" : hpPercent >= 20 ? "#F1C44F" : "#EC5132");
     return (
         <Stack direction="row" spacing={1} justifyContent="center" alignItems="center" sx={{ width: "100%" }}>
-            <Box sx={{ width: 125 }}>
+            <Box sx={{ width: 150 }}>
                 <Stack direction="row">
                     <Box flexGrow={1}/>
                     <Typography>
@@ -58,18 +58,71 @@ function HpDisplayLine({role, curhp, maxhp}: {role: string, curhp: number, maxhp
                     {`${curhp}` + " / " + `${maxhp}`}
                 </Typography>
             </Box>
+            <Box sx={{ width: 150 }}>
+                <Typography>
+                    {kos > 0 ? (`${kos}` + (kos > 1 ? " KOs" : " KO")) : ""}
+                </Typography>
+            </Box>
         </Stack>
     );
 }
 
-function HpDisplay({roles, currenthps, maxhps}: {roles: string[], currenthps: number[], maxhps: number[]}) {
+function HpDisplay({results}: {results: RaidBattleResults}) {
+    const [displayedTurn, setDisplayedTurn] = useState<number>(0);
+    const [snapToEnd, setSnapToEnd] = useState<boolean>(true);
+    const maxhps = results.endState.raiders.map((raider) => ( raider.maxHP === undefined ? new Pokemon(9, raider.name, {...raider}).maxHP() : raider.maxHP()) );
+    
+    const turnState = displayedTurn === 0 ? results.endState : results.turnResults[Math.min(results.turnResults.length, displayedTurn) - 1].state;
+    const currenthps = displayedTurn === 0 ? maxhps : turnState.raiders.map((raider) => raider.originalCurHP); 
+
+    const koCounts = [0,1,2,3,4].map((i) => results.turnResults.slice(0,displayedTurn).reduce((kos, turn, idx) => 
+            kos + ((turn.state.raiders[i].originalCurHP === 0 && (i === 0 || turn.moveInfo.userID === i)) ? 1 : 0),
+        0));
+    const roles = results.endState.raiders.map((raider) => raider.role);
+
+    console.log(koCounts)
+
+    useEffect(() => { 
+        if (snapToEnd || displayedTurn > results.turnResults.length) {
+            setDisplayedTurn(results.turnResults.length);
+        }
+    }, [results.turnResults.length]);
+
+    useEffect(() => {
+        if (displayedTurn === results.turnResults.length) {
+            setSnapToEnd(true);
+        } else {
+            setSnapToEnd(false);
+        }
+    }, [displayedTurn]);
+
     return (
-        <Stack spacing={1.5} sx={{marginBottom: 1}}>
-            <HpDisplayLine role={roles[0]} curhp={currenthps[0]} maxhp={maxhps[0]} />
-            <HpDisplayLine role={roles[1]} curhp={currenthps[1]} maxhp={maxhps[1]} />
-            <HpDisplayLine role={roles[2]} curhp={currenthps[2]} maxhp={maxhps[2]} />
-            <HpDisplayLine role={roles[3]} curhp={currenthps[3]} maxhp={maxhps[3]} />
-            <HpDisplayLine role={roles[4]} curhp={currenthps[4]} maxhp={maxhps[4]} />
+        <Stack spacing={1} sx={{marginBottom: 2}}>
+            <HpDisplayLine role={roles[0]} curhp={currenthps[0]} maxhp={maxhps[0]} kos={koCounts[0]} />
+            <HpDisplayLine role={roles[1]} curhp={currenthps[1]} maxhp={maxhps[1]} kos={koCounts[1]} />
+            <HpDisplayLine role={roles[2]} curhp={currenthps[2]} maxhp={maxhps[2]} kos={koCounts[2]} />
+            <HpDisplayLine role={roles[3]} curhp={currenthps[3]} maxhp={maxhps[3]} kos={koCounts[3]} />
+            <HpDisplayLine role={roles[4]} curhp={currenthps[4]} maxhp={maxhps[4]} kos={koCounts[4]} />
+            <br/>
+            <Stack direction="row" spacing={3} justifyContent="center" alignItems="center" sx={{ width: "100%" }}>
+                <Box sx={{ width: 115 }}>
+                    <Stack direction="row">
+                        <Box flexGrow={1}/>
+                        <Typography>
+                            {displayedTurn === 0 ? "Battle Start" : "Turn " + displayedTurn}
+                        </Typography>
+                    </Stack>
+                </Box>
+                <Slider 
+                    value={displayedTurn}
+                    onChange={(event, newValue) => setDisplayedTurn(newValue as number)}
+                    step={1}
+                    marks
+                    min={0}
+                    max={results.turnResults.length}
+                />
+                <Box sx={{ width: 50 }} />
+            </Stack>
         </Stack>
     )
 }
@@ -126,7 +179,7 @@ function RaidControls({raidInputProps, results, setResults, prettyMode}: {raidIn
                     </Tabs>
                 </Box>
                 <Box hidden={value !== 1}>
-                    <HpDisplay roles={roles} currenthps={currenthps} maxhps={maxhps} />
+                    <HpDisplay results={results} />
                 </Box>
                 <Box hidden={value !== 1}>
                     <Box sx={{ height: 560, overflowY: "auto" }}>

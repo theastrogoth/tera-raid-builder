19c743ab29649c91cd40074f9cc9fd84ff36ccce
theAstroGoth <theAstroGoth@gmail.com>
2024-01-05 17:32:26 -0600

    Small fixes to species/move data

--- get_all_species.jl
+++ get_all_species.jl
@@ -10,6 +10,27 @@ const IGNORED_ALT_FORMS = [
     "Terapagos-Stellar",
 ]
 
+const TYPES = [ # for Arceus, ignore normal
+    # "Normal", 
+    "Fire",
+    "Water",
+    "Grass",
+    "Electric",
+    "Ice",
+    "Fighting",
+    "Poison",
+    "Ground",
+    "Flying",
+    "Psychic",
+    "Bug",
+    "Rock",
+    "Ghost",
+    "Dragon",
+    "Dark",
+    "Steel",
+    "Fairy",
+]
+
 function name_to_filename(name)
     words = split(string(name), [' ','-'])
     filename = join(words, "-")
@@ -38,10 +59,33 @@ species_path = "data/species.json"
 read_path = "data/pokemon"
 write_path = "data/pokemon"
 
-all_species_data = JSON3.read(species_path)
-species = [ string(key) for (key,data) in all_species_data ]
-
 files = readdir(read_path)
+species_from_file = String[]
+
+for file in files
+    if file == "_allspecies.json"
+        continue
+    end
+    path = joinpath(read_path, file)
+    data = JSON3.read(path)
+    name = data[:name]
+    push!(species_from_file, name)
+end
+
+all_species_data = JSON3.read(species_path)
+species = String[]
+mismatched_species = String[]
+for (key, data) in all_species_data
+    push!(species, string(key))
+    if string(key) ∉ species_from_file
+        push!(mismatched_species, string(key))
+    end
+end
+
+if length(mismatched_species) > 0
+    @warn "There are $(length(mismatched_species)) species in species.json that are not in the pokemon data files."
+    @show mismatched_species
+end
 
 all_species_dict = OrderedDict{String,Any}()
 for file in files
@@ -51,6 +95,8 @@ for file in files
     path = joinpath(read_path, file)
     data = JSON3.read(path)
     name = data[:name]
+    name = occursin("Flab", name) ? "Flabébé" : name
+    
     if name in IGNORED_ALT_FORMS
         continue
     end
@@ -61,7 +107,7 @@ for file in files
     species_data = all_species_data[name]
 
     abilities = [ OrderedDict{String,Union{String,Bool}}([string(key) => val for (key,val) in entry]...) for entry in data[:abilities] ]
-    types = [string(type) for type in data[:types]]
+    types = [uppercasefirst(string(type)) for type in data[:types]]
     stats = OrderedDict{String,Int}()
     for (key,val) in data[:stats]
         stats[string(key)] = val
@@ -73,10 +119,21 @@ for file in files
 
     poke_data = PokemonData(name, abilities, types, stats, weightkg, nfe, gender, moves)
     all_species_dict[name] = poke_data
+
+    if name === "Arceus"
+        for type in TYPES
+            new_name = "Arceus-$(type)"
+            new_filename = name_to_filename(new_name)*".json";
+            new_path = joinpath(write_path, new_filename);
+            new_types = [type]
+            new_poke_data = PokemonData(new_name, abilities, new_types, stats, weightkg, nfe, gender, moves)
+            all_species_dict[new_name] = new_poke_data
+        end
+    end
 end
 sort!(all_species_dict)
 
-## write master list of species
+# write master list of species
 open("$write_path/_allspecies.json", "w") do io
     JSON3.pretty(io, all_species_dict)
 end

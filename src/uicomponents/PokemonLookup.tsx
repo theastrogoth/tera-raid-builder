30cc7377091126a57a8179f2016976605b8f43f0
theAstroGoth <theAstroGoth@gmail.com>
2024-01-13 19:03:11 -0600

    Added Type colors for tags, translation support for popover

--- src/uicomponents/PokemonLookup.tsx
+++ src/uicomponents/PokemonLookup.tsx
@@ -41,6 +41,27 @@ import RAIDER_SETDEX_SV from "../data/sets/raiders.json";
 import { Chip, Divider } from "@mui/material";
 import { findOptionFromPokemonName } from "./BuildControls";
 
+const TYPE_COLORS = {
+    "Bug": "#A2A400",
+    "Dark": "#484848",
+    "Dragon": "#5463E8",
+    "Electric": "#CCAD00",
+    "Fairy": "#CC89CC",
+    "Fighting": "#FF9E00",
+    "Fire": "#FF5715",
+    "Flying": "#6CA2CC",
+    "Ghost": "#733E77",
+    "Grass": "#00C300",
+    "Ground": "#B37531",
+    "Ice": "#00ADCC",
+    "Normal": "#999999",
+    "Poison": "#A44BD6",
+    "Psychic": "#FF4D78",
+    "Rock": "#97996B",
+    "Steel": "#4DB3D8",
+    "Water": "#0095FF",
+};
+
 const gen = Generations.get(9);
 const genTypes = [...gen.types].map(type => type.name).filter((t) => t !== "Stellar" && t !== "???").sort();
 const genAbilities = ABILITIES[gen.num].filter((a) => a !== "(No Ability)").sort();
@@ -335,34 +356,42 @@ function checkOptionAgainstInput(option: SearchOption, inputValue: string, trans
     return normalizedOptionName.includes(normalizedInputValue);
 }
 
-function FilterGenericTag({text, handleDelete}: {text: String, handleDelete: () => void}) {
+function FilterGenericTag({text, handleDelete}: {text: String, handleDelete?: () => void}) {
     return (
-        <Paper elevation={0} variant='outlined' sx={{ borderRadius: 100 }}> 
-            <Stack direction="row" spacing={0.5} justifyContent="center" alignItems="center" sx={{ paddingLeft: "10px", paddingRight: "5px" }}>
-                <Typography variant="body1" justifyContent="center" alignItems="center">
+        <ShadedPaper elevation={0} variant='outlined' sx={{ borderRadius: 100 }}> 
+            <Stack direction="row" spacing={0.5} justifyContent="center" alignItems="center" sx={{ minWidth: "50px", paddingX: "10px", paddingRight: handleDelete ? "5px" : "10px" }}>
+                <Typography variant="body1" justifyContent="center" alignItems="center" sx={{ paddingY: "5px"}}>
                     {text}
                 </Typography>
-                <Box flexGrow={1} />
-                <IconButton size="small" onClick={handleDelete}>
-                    <CloseIcon/>
-                </IconButton>
+                { handleDelete &&
+                <>
+                    <Box flexGrow={1} />
+                    <IconButton size="small" onClick={handleDelete}>
+                        <CloseIcon/>
+                    </IconButton>
+                </>
+                }
             </Stack>
-        </Paper>
+        </ShadedPaper>
     );
 }
 
-function FilterTypeTag({text, handleDelete}: {text: String, handleDelete: () => void}) {
+function FilterTypeTag({text, color, handleDelete}: {text: String, color: String, handleDelete?: () => void}) {
     return (
-        // TODO: Add type colors
-        <Paper elevation={0} variant='outlined' sx={{ borderRadius: 100 }}> 
-            <Stack direction="row" spacing={0.5} justifyContent="center" alignItems="center" sx={{ paddingLeft: "10px", paddingRight: "5px" }}>
-                <Typography variant="body1" justifyContent="center" alignItems="center">
+        // @ts-ignore
+        <Paper elevation={0} variant='outlined' sx={{ borderRadius: 100, backgroundColor: color }}> 
+            <Stack direction="row" spacing={0.5} justifyContent="center" alignItems="center" sx={{ minWidth: "50px", paddingX: "10px", paddingRight: handleDelete ? "5px" : "10px" }}>
+                <Typography variant="body1" justifyContent="center" alignItems="center" color={"white"} sx={{ paddingY: "5px"}}>
                     {text}
                 </Typography>
-                <Box flexGrow={1} />
-                <IconButton size="small" onClick={handleDelete}>
-                    <CloseIcon/>
-                </IconButton>
+                { handleDelete &&
+                <>
+                    <Box flexGrow={1} />
+                    <IconButton size="small" onClick={handleDelete}>
+                        <CloseIcon/>
+                    </IconButton>
+                </>
+                }
             </Stack>
         </Paper>
     );
@@ -371,11 +400,14 @@ function FilterTypeTag({text, handleDelete}: {text: String, handleDelete: () => 
 function FilterTagDispatcher({filter, handleDelete, translationKey}: {filter: SearchOption, handleDelete: () => void, translationKey: any}) {
     switch(filter.type) {
         case "Type":
-            return <FilterTypeTag text={getTranslation(filter.name, translationKey, "types")} handleDelete={handleDelete}/>
+            // @ts-ignore
+            return <FilterTypeTag text={getTranslation(filter.name, translationKey, "types")} color={TYPE_COLORS[filter.name]} handleDelete={handleDelete}/>
         case "Moves":
-            return <FilterTypeTag text={getTranslation(filter.name, translationKey, "moves")} handleDelete={handleDelete}/>
+            const move = gen.moves.get(toID(filter.name))!;
+            // @ts-ignore
+            return <FilterTypeTag text={getTranslation(filter.name, translationKey, "moves")} color={TYPE_COLORS[move.type]} handleDelete={handleDelete}/>
         case "Ability":
-            return <FilterTypeTag text={getTranslation(filter.name, translationKey, "abilities")} handleDelete={handleDelete}/>
+            return <FilterGenericTag text={getTranslation(filter.name, translationKey, "abilities")} handleDelete={handleDelete}/>
         case "Custom":
             return <FilterGenericTag text={getTranslation(filter.name, translationKey, "ui")} handleDelete={handleDelete}/>
         default:
@@ -392,6 +424,14 @@ function FilterTags({filters, translationKey, handleDeleteFilter}: {filters: Sea
         </Stack>
     );
 }
+
+const ShadedPaper = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'light' ? darken(theme.palette.background.paper, 0.1) : lighten(theme.palette.background.paper, 0.1),
+}));    
+
+const ShadedChip = styled(Chip)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'light' ? darken(theme.palette.background.paper, 0.1) : lighten(theme.palette.background.paper, 0.1),
+}));
 
 const ButtonTable = styled(Table)(({ theme }) => ({
     size: "small",
@@ -1272,66 +1312,75 @@ function PokemonLookup({loadSet, allSpecies, allMoves, setAllSpecies, setAllMove
                                         <Stack direction="column">
                                             <Box>
                                                 <Typography variant="h6">
-                                                    Search Info
+                                                    {`${getTranslation("Search Info",translationKey)}`}
                                                 </Typography>
                                             </Box>
                                             <Divider/>
                                             <Box padding="10px 0px">
                                                 <Typography fontWeight="bold">
-                                                    Simple Filters:
+                                                    {`${getTranslation("Simple Filters",translationKey)}:`}
                                                 </Typography>
                                                 <Stack direction="column">
                                                     <Stack direction="row" paddingLeft="20px" alignItems="center" margin="5px 0px">
-                                                        <Typography>By Type:</Typography>
-                                                        <Chip label="Fairy" variant="outlined" size="small" sx={{"marginLeft": "10px"}}/>
+                                                        <Typography>{`${getTranslation("By Type",translationKey)}:`}</Typography>
+                                                        <Box flexGrow={1} sx={{minWidth: "5px"}} />
+                                                        <FilterTypeTag text={getTranslation("Fairy",translationKey,"types")} color={TYPE_COLORS.Fairy} />
                                                     </Stack>
                                                     <Stack direction="row" paddingLeft="20px" alignItems="center" margin="5px 0px">
-                                                        <Typography>By Ability:</Typography>
-                                                        <Chip label="Friend Guard" variant="outlined" size="small" sx={{"marginLeft": "10px"}}/>
+                                                        <Typography>{`${getTranslation("By Ability",translationKey)}:`}</Typography>
+                                                        <Box flexGrow={1} sx={{minWidth: "5px"}} />
+                                                        <FilterGenericTag text={getTranslation("Friend Guard",translationKey,"abilities")} />
                                                     </Stack>
                                                     <Stack direction="row" paddingLeft="20px" alignItems="center" margin="5px 0px">
-                                                        <Typography>By Move:</Typography>
-                                                        <Chip label="Helping Hand" variant="outlined" size="small" sx={{"marginLeft": "10px"}}/>
+                                                        <Typography>{`${getTranslation("By Move",translationKey)}:`}</Typography>
+                                                        <Box flexGrow={1} sx={{minWidth: "5px"}} />
+                                                        <FilterTypeTag text={getTranslation("Helping Hand",translationKey,"moves")} color={TYPE_COLORS.Normal} />
                                                     </Stack>
                                                     <Stack direction="row" paddingLeft="20px" alignItems="center" margin="5px 0px">
-                                                        <Typography>By Stat:</Typography>
-                                                        <Chip label="Speed > 95" variant="outlined" size="small" sx={{"marginLeft": "10px"}}/>
+                                                        <Typography>{`${getTranslation("By Stat",translationKey)}:`}</Typography>
+                                                        <Box flexGrow={1} sx={{minWidth: "5px"}} />
+                                                        <FilterGenericTag text={`${getTranslation("Speed",translationKey,"stats")} > 95`} />
                                                     </Stack>
                                                 </Stack>
                                             </Box>
                                             <Box padding="10px 0px">
                                                 <Typography fontWeight="bold">
-                                                    Custom Filters:
+                                                    {`${getTranslation("Custom Filters",translationKey)}:`}
                                                 </Typography>
                                                 <Stack direction="column">
                                                     <Stack direction="column" paddingLeft="20px" margin="5px 0px">
-                                                        <Typography>And Operator:</Typography>
-                                                        <Chip label="Intimidate and Fire" variant="outlined" size="small" sx={{"width": "fit-content"}}/>
+                                                        <Typography>{`${getTranslation("And Operator",translationKey)}:`}</Typography>
+                                                        <FilterGenericTag text={`${getTranslation("Intimidate",translationKey,"abilities")} ${getTranslation("and",translationKey)} ${getTranslation("Fire",translationKey,"types")}`} />
                                                     </Stack>
                                                 </Stack>
                                                 <Stack direction="column" paddingLeft="20px" margin="5px 0px">
-                                                    <Typography>Or Operator:</Typography>
-                                                    <Chip label="Fake Tears or Acid Spray" variant="outlined" size="small" sx={{"width": "fit-content"}}/>
+                                                    <Typography>{`${getTranslation("Or Operator",translationKey)}:`}</Typography>
+                                                    <FilterGenericTag text={`${getTranslation("Fake Tears",translationKey,"moves")} ${getTranslation("or",translationKey)} ${getTranslation("Acid Spray",translationKey,"moves")}`} />
                                                 </Stack>
                                             </Box>
                                             <Box padding="10px 0px">
                                                 <Typography fontWeight="bold">
-                                                    Complex Filters:
+                                                    {`${getTranslation("Complex Filters",translationKey)}:`}
                                                 </Typography>
                                                 <Stack direction="column">
                                                     <Stack direction="column" paddingLeft="20px" margin="5px 0px">
                                                         <Typography>Queries:</Typography>
-                                                        <Chip 
+                                                        <ShadedChip 
                                                             label={
                                                                 <Typography style={{"whiteSpace":"normal", "fontSize":"0.6384rem", "textAlign":"center"}}>
-                                                                     Flying and Screech and <br/>(Speed {'<'} 80 or Compound Eyes)
+                                                                    {/* Flying and Screech and <br/>(Speed {'<'} 80 or Compound Eyes) */}
+                                                                    {`${getTranslation("Flying",translationKey,"types")} ${getTranslation("and",translationKey)} ${getTranslation("Screech",translationKey,"moves")} ${getTranslation("and",translationKey)}`}
+                                                                    <br/>
+                                                                    {`(${getTranslation("Speed",translationKey,"stats")} < 80 ${getTranslation("or",translationKey)} ${getTranslation("Compound Eyes",translationKey,"abilities")})`}
                                                                 </Typography>
                                                             }
                                                             variant="outlined" size="small" 
                                                             sx={{
+                                                                "borderRadius": "100px",
                                                                 "width": "170px",
                                                                 "height": "48px"
                                                             }}/>
+                                                        {/* <FilterGenericTag text={`${getTranslation("Flying",translationKey,"types")} ${getTranslation("and",translationKey)} ${getTranslation("Screech",translationKey,"moves")} ${getTranslation("and",translationKey)} (${getTranslation("Speed",translationKey,"stats")} < 80 ${getTranslation("or",translationKey)} ${getTranslation("Compound Eyes",translationKey,"abilities")})`} /> */}
                                                     </Stack>
                                                 </Stack>
                                             </Box>

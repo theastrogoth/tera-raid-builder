ba57b77564acc1db807f3f408f86dfbde8cbefd8
TheAstrogoth <theastrogoth@gmail.com>
2024-06-11 18:34:21 -0500

    Improved selection/search for held-item dependant forms - Arceus only has one entry in the species select field and search box - Ogerpon gets one entry in the species select field - Origin forms removed from species select field - Crowned forms removed from species select field - Forms change appropriately when given a relevant held item

--- src/uicomponents/BuildControls.tsx
+++ src/uicomponents/BuildControls.tsx
@@ -49,6 +49,7 @@ import BOSS_SETDEX_TM from "../data/sets/tm_raid_bosses.json";
 import BOSS_SETDEX_ID from "../data/sets/id_raid_bosses.json";
 
 import PokemonLookup from "./PokemonLookup";
+import { getItemBoostType } from "../calc/items";
 
 
 // we will always use Gen 9
@@ -56,7 +57,42 @@ const gen = Generations.get(9);
 const genTypes = [...gen.types].map(type => type.name).sort();
 const genItems = ["(No Item)", ...[...gen.items].map(item => item.name).sort()];
 const genNatures = [...gen.natures].sort();
-const genSpecies = [...gen.species].map(specie => specie.name).filter((n) => !["Mimikyu-Busted", "Minior-Meteor", "Eiscue-Noice", "Morpeko-Hangry", "Terapagos-Stellar", "Meloetta-Pirouette"].includes(n)).sort();
+
+const UNSELECTABLE_FORMS = [
+    "Mimikyu-Busted", 
+    "Minior-Meteor", 
+    "Eiscue-Noice", 
+    "Morpeko-Hangry", 
+    "Terapagos-Stellar", 
+    "Meloetta-Pirouette",
+    "Zacian-Crowned",
+    "Zamazenta-Crowned",
+    "Dialga-Origin",
+    "Palkia-Origin",
+    "Giratina-Origin",
+    "Ogerpon-Hearthflame",
+    "Ogerpon-Wellspring",
+    "Ogerpon-Cornerstone",
+    "Arceus-Bug",
+    "Arceus-Dark",
+    "Arceus-Dragon",
+    "Arceus-Electric",
+    "Arceus-Fairy",
+    "Arceus-Fighting",
+    "Arceus-Fire",
+    "Arceus-Flying",
+    "Arceus-Ghost",
+    "Arceus-Grass",
+    "Arceus-Ground",
+    "Arceus-Ice",
+    "Arceus-Psychic",
+    "Arceus-Poison",
+    "Arceus-Rock",
+    "Arceus-Steel",
+    "Arceus-Water",
+];
+
+const genSpecies = [...gen.species].map(specie => specie.name).filter((n) => !UNSELECTABLE_FORMS.includes(n)).sort();
 
 // const raiderSetOptions = setdexToOptions(RAIDER_SETDEX_SV);
 const bossSetOptions = [...setdexToOptions(BOSS_SETDEX_SV), ...setdexToOptions(BOSS_SETDEX_TM), ...setdexToOptions(BOSS_SETDEX_ID)].sort((a,b) => (a.pokemon + a.name) < (b.pokemon + b.name) ? -1 : 1);
@@ -1061,67 +1097,152 @@ function BuildControls({pokemon, abilities, moveSet, setPokemon, substitutes, se
     useEffect(() => {
         // Locked items/teratypes
         if (pokemon.name.includes("Ogerpon")) {
-            if (pokemon.name === "Ogerpon") {
+            if (pokemon.name !== "Ogerpon" && !pokemon.hasItem("Hearthflame Mask", "Wellspring Mask", "Cornerstone Mask")) {
                 setTeraTypes(["Grass"]);
-                setPokemonProperties(["role", "teraType"])(["Ogerpon", "Grass"]);
-            } else if (pokemon.name === "Ogerpon-Hearthflame") {
+                setItems(genItems);
+                const poke = pokemon.clone();
+                poke.teraType = "Grass";
+                poke.ability = "Defiant" as AbilityName;
+                handleForcedChangeSpecies("Ogerpon", poke);
+            } else if (pokemon.name === "Ogerpon-Hearthflame" || pokemon.hasItem("Hearthflame Mask")) {
+                console.log("here")
                 setTeraTypes(["Fire"]);
-                setItems(["Hearthflame Mask" as ItemName]);
-                setPokemonProperties(["role", "teraType", "item"])(["Ogerpon", "Fire", "Hearthflame Mask"]);
-            } else if (pokemon.name === "Ogerpon-Wellspring") {
+                setItems(["(No Item)", "Hearthflame Mask" as ItemName]);
+                const poke = pokemon.clone();
+                poke.teraType = "Fire";
+                poke.item = "Hearthflame Mask" as ItemName;
+                poke.ability = "Mold Breaker" as AbilityName;
+                handleForcedChangeSpecies("Ogerpon-Hearthflame", poke);
+            } else if (pokemon.name === "Ogerpon-Wellspring" || pokemon.hasItem("Wellspring Mask")) {
                 setTeraTypes(["Water"]);
-                setItems(["Wellspring Mask" as ItemName]);
-                setPokemonProperties(["role", "teraType", "item"])(["Ogerpon", "Water", "Wellspring Mask"]);
-            } else { // (pokemmon.name === "Ogerpon-Cornerstone")
+                setItems(["(No Item)", "Wellspring Mask" as ItemName]);
+                const poke = pokemon.clone();
+                poke.teraType = "Water";
+                poke.item = "Wellspring Mask" as ItemName;
+                poke.ability = "Water Absorb" as AbilityName;
+                handleForcedChangeSpecies("Ogerpon-Wellspring", poke);
+            } else if (pokemon.name === "Ogerpon-Cornerstone" || pokemon.hasItem("Cornerstone Mask")) {
                 setTeraTypes(["Rock"]);
-                setItems(["Cornerstone Mask" as ItemName]);
-                setPokemonProperties(["role", "teraType", "item"])(["Ogerpon", "Rock", "Cornerstone Mask"]);
+                setItems(["(No Item)", "Cornerstone Mask" as ItemName]);
+                const poke = pokemon.clone();
+                poke.teraType = "Rock";
+                poke.item = "Cornerstone Mask" as ItemName;
+                poke.ability = "Sturdy" as AbilityName;
+                handleForcedChangeSpecies("Ogerpon-Cornerstone", poke);
+            } else if (teraTypes.length !== 1 || teraTypes[0] !== "Grass") {
+                setTeraTypes(["Grass"]);
+                setItems(genItems);
             }
-        } else if (pokemon.name === "Zacian-Crowned") {
-            setItems(["Rusted Sword" as ItemName]);
-            setPokemonProperties(["role","item"])(["Zacian", "Rusted Sword"]);
-        } else if (pokemon.name === "Zamazenta-Crowned") {
-            setItems(["Rusted Shield" as ItemName]);
-            setPokemonProperties(["role","item"])(["Zamazenta", "Rusted Shield"]);
+        } else if (pokemon.name.includes("Zacian")) {
+            if (pokemon.name !== "Zacian" && !pokemon.hasItem("Rusted Sword")) {
+                setItems(genItems);
+                const poke = pokemon.clone();
+                handleForcedChangeSpecies("Zacian", poke);
+            } else if (pokemon.name === "Zacian-Crowned" || pokemon.hasItem("Rusted Sword")) {
+                setItems(["(No Item)", "Rusted Sword" as ItemName]);
+                const poke = pokemon.clone();
+                poke.item = "Rusted Sword" as ItemName;
+                handleForcedChangeSpecies("Zacian-Crowned", poke);
+            } else {
+                setItems(genItems);
+            }
+        } else if (pokemon.name.includes("Zamazenta")) {
+            if (pokemon.name !== "Zamazenta" && !pokemon.hasItem("Rusted Shield")) {
+                setItems(genItems);
+                const poke = pokemon.clone();
+                handleForcedChangeSpecies("Zamazenta", poke);
+            } else if (pokemon.name === "Zamazenta-Crowned") {
+                setItems(["(No Item)", "Rusted Shield" as ItemName]);
+                const poke = pokemon.clone();
+                poke.item = "Rusted Shield" as ItemName;
+                handleForcedChangeSpecies("Zamazenta-Crowned", poke);
+            } else {
+                setItems(genItems);
+            }
         } else if (pokemon.name.includes("Terapagos")) {
             setTeraTypes(["Stellar"]);
             setPokemonProperties(["teraType"])(["Stellar"]);
-        } else if (pokemon.name.includes("Dialga-Origin")) {
-            setItems(["Adamant Crystal"]);
-            setPokemonProperties(["role","item"])(["Dialga", "Adamant Crystal"]);
-        } else if (pokemon.name.includes("Palkia-Origin")) {
-            setItems(["Lustrous Globe"]);
-            setPokemonProperties(["role","item"])(["Palkia", "Lustrous Globe"]);
-        } else if (pokemon.name.includes("Giratina-Origin")) {
-            setItems(["Griseous Orb", "Griseous Core"]);
-            // setPokemonProperties(["role","item"])(["Giratina", "Griseous Core"]);
+        } else if (pokemon.name.includes("Dialga")) {
+            if (pokemon.name !== "Dialga" && !pokemon.hasItem("Adamant Crystal")) {
+                setItems(genItems);
+                const poke = pokemon.clone();
+                handleForcedChangeSpecies("Dialga", poke);
+            } else if (pokemon.name === "Dialga-Origin" || pokemon.hasItem("Adamant Crystal")) {
+                setItems(["(No Item)", "Adamant Crystal"]);
+                const poke = pokemon.clone();
+                poke.item = "Adamant Crystal" as ItemName;
+                handleForcedChangeSpecies("Dialga-Origin", poke);
+            } else {
+                setItems(genItems);
+            }
+        } else if (pokemon.name.includes("Palkia")) {
+            if (pokemon.name !== "Palkia" && !pokemon.hasItem("Lustrous Globe")) {
+                setItems(genItems);
+                const poke = pokemon.clone();
+                handleForcedChangeSpecies("Palkia", poke);
+            } else if (pokemon.name === "Palkia-Origin" || pokemon.hasItem("Lustrous Globe")) {
+                setItems(["(No Item)", "Lustrous Globe"]);
+                const poke = pokemon.clone();
+                poke.item = "Lustrous Globe" as ItemName;
+                handleForcedChangeSpecies("Palkia-Origin", poke);
+            } else {
+                setItems(genItems);
+            }
+        } else if (pokemon.name.includes("Giratina")) {
+            if (pokemon.name !== "Giratina" && !pokemon.hasItem("Griseous Orb", "Griseous Core")) {
+                setItems(genItems);
+                const poke = pokemon.clone();
+                handleForcedChangeSpecies("Giratina", poke);
+            } else if (pokemon.name === "Giratina-Origin" || pokemon.hasItem("Griseous Orb", "Griseous Core")) {
+                setItems(["(No Item)", "Griseous Orb", "Griseous Core"]);
+                const poke = pokemon.clone();
+                poke.item = pokemon.hasItem("Griseous Orb", "Griseous Core") ? pokemon.item : "Griseous Orb" as ItemName;
+                handleForcedChangeSpecies("Giratina-Origin", poke);
+            } else {
+                setItems(genItems);
+            }
         }
         // Arceus Plate Types
         else if (pokemon.name.includes("Arceus")) {
-            if (pokemon.name.includes("-")) {
-                const plateType = pokemon.name.split("-")[1];
-                let plateItem = pokemon.item || "";
-                switch (plateType) {
-                    case "Dragon": plateItem = "Draco Plate"; break;
-                    case "Dark": plateItem = "Dread Plate"; break;
-                    case "Electric": plateItem = "Zap Plate"; break;
-                    case "Fairy": plateItem = "Pixie Plate"; break;
-                    case "Fighting": plateItem = "Fist Plate"; break;
-                    case "Fire": plateItem = "Flame Plate"; break;
-                    case "Flying": plateItem = "Sky Plate"; break;
-                    case "Ghost": plateItem = "Spooky Plate"; break;
-                    case "Grass": plateItem = "Meadow Plate"; break;
-                    case "Ground": plateItem = "Earth Plate"; break;
-                    case "Ice": plateItem = "Icicle Plate"; break;
-                    case "Poison": plateItem = "Toxic Plate"; break;
-                    case "Psychic": plateItem = "Mind Plate"; break;
-                    case "Rock": plateItem = "Stone Plate"; break;
-                    case "Steel": plateItem = "Iron Plate"; break;
-                    case "Water": plateItem = "Splash Plate"; break;                 
+            if (pokemon.name !== "Arceus" && !pokemon.item?.includes("Plate")) {
+                setItems(genItems);
+                const poke = pokemon.clone();
+                handleForcedChangeSpecies("Arceus", poke);
+            } else if (pokemon.name.includes("Arceus-") || pokemon.item?.includes("Plate")) {
+                let plateType = "";
+                if (pokemon.name === "Arceus") {
+                    plateType = getItemBoostType(pokemon.item as ItemName) || "";
+                } else {
+                    plateType = pokemon.name.split("-")[1];
                 }
-                setItems([plateItem as ItemName]);
-                setPokemonProperties(["role", "item", "ability"])(["Arceus", plateItem, "Multitype"]);
-            } 
+                let plateItem = pokemon.item || "";
+                let form: string = pokemon.name;
+                switch (plateType) {
+                    case "Bug": plateItem       = "Insect Plate";   form = "Arceus-Bug";        break;
+                    case "Dark": plateItem      = "Dread Plate";    form = "Arceus-Dark";       break;
+                    case "Dragon": plateItem    = "Draco Plate";    form = "Arceus-Dragon";     break;
+                    case "Electric": plateItem  = "Zap Plate";      form = "Arceus-Electric";   break;
+                    case "Fairy": plateItem     = "Pixie Plate";    form = "Arceus-Fairy";      break;
+                    case "Fighting": plateItem  = "Fist Plate";     form = "Arceus-Fighting";   break;
+                    case "Fire": plateItem      = "Flame Plate";    form = "Arceus-Fire";       break;
+                    case "Flying": plateItem    = "Sky Plate";      form = "Arceus-Flying";     break;
+                    case "Ghost": plateItem     = "Spooky Plate";   form = "Arceus-Ghost";      break;
+                    case "Grass": plateItem     = "Meadow Plate";   form = "Arceus-Grass";      break;
+                    case "Ground": plateItem    = "Earth Plate";    form = "Arceus-Ground";     break;
+                    case "Ice": plateItem       = "Icicle Plate";   form = "Arceus-Ice";        break;
+                    case "Poison": plateItem    = "Toxic Plate";    form = "Arceus-Poison";     break;
+                    case "Psychic": plateItem   = "Mind Plate";     form = "Arceus-Psychic";    break;
+                    case "Rock": plateItem      = "Stone Plate";    form = "Arceus-Rock";       break;
+                    case "Steel": plateItem     = "Iron Plate";     form = "Arceus-Steel";      break;
+                    case "Water": plateItem     = "Splash Plate";   form = "Arceus-Water";      break;                 
+                }
+                setItems(["(No Item)", plateItem as ItemName]);
+                const poke = pokemon.clone();
+                poke.item = plateItem as ItemName;
+                handleForcedChangeSpecies(form as SpeciesName, poke);
+            } else {
+                setItems(genItems);
+            }
         } else {
             setTeraTypes(genTypes);
             setItems(genItems);
@@ -1224,6 +1345,30 @@ function BuildControls({pokemon, abilities, moveSet, setPokemon, substitutes, se
                 },
             }), 
             [],
+        ));
+    }
+
+    const handleForcedChangeSpecies = (val: string, poke: Raider) => {
+        setPokemon(new Raider(
+            poke.id, 
+            poke.role, 
+            poke.shiny, 
+            false,
+            poke.field, 
+            new Pokemon(gen, val, {
+                nature: poke.nature, 
+                level: poke.level,
+                ability: poke.ability,
+                teraType: poke.teraType,
+                item: poke.item,
+                evs: poke.evs,
+                ivs: poke.ivs,
+                moves: poke.moves,
+                shieldData: poke.shieldData,
+            }), 
+            poke.moveData,
+            poke.extraMoves,
+            poke.extraMoveData,
         ));
     }
 

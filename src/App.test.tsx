adf97b5b31e0e35c7ef81022bb1940a60980295d
theAstroGoth <theastrogoth@gmail.com>
2025-02-28 10:29:07 -0600

    Updated main strats list for tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -38,7 +38,8 @@ const MAIN_STRATS = [ // manually include ones that aren't named "main"
   'infernape/rock_all_night_3',
   'feraligatr/latr_gatr_3',
   'torterra/cryogre_3',
-  'rayquaza/cakey_snakey_3'
+  'rayquaza/cakey_snakey_3',
+  'meowscarada/catfished_3'
 ]
 
 for (const [_, list] of Object.entries(STRAT_LIST)) { // collect all the ones named "main"

729e9db0aafe423c93f0c68c0c41a5c98b153610
TheAstrogoth <theastrogoth@gmail.com>
2026-03-05 20:48:31 -0600

    Swampert rerun

--- src/uicomponents/StratLoadField.tsx
+++ src/uicomponents/StratLoadField.tsx
@@ -29,7 +29,7 @@ type StratOption = {
     credits?: string,
 }
 
-const SHOW_BOSSES_FIRST: SpeciesName[] = ["Blaziken"] as SpeciesName[];
+const SHOW_BOSSES_FIRST: SpeciesName[] = ["Swampert"] as SpeciesName[];
 
 function stratDexEntryToOption(options: StratOption[], index: number, boss: string, stratname: string, stratpath: string) {
     import(`../data/strats/${stratpath}.json`)

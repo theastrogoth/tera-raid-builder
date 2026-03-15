19dbcac573e77f788b284dc15606d1e425ada166
EZ <ericzklm@hotmail.com>
2026-03-15 00:50:38 -0700

    Update Torterra Rerun

--- src/uicomponents/StratLoadField.tsx
+++ src/uicomponents/StratLoadField.tsx
@@ -29,7 +29,7 @@ type StratOption = {
     credits?: string,
 }
 
-const SHOW_BOSSES_FIRST: SpeciesName[] = ["Swampert"] as SpeciesName[];
+const SHOW_BOSSES_FIRST: SpeciesName[] = ["Torterra"] as SpeciesName[];
 
 function stratDexEntryToOption(options: StratOption[], index: number, boss: string, stratname: string, stratpath: string) {
     import(`../data/strats/${stratpath}.json`)

0eba21543cc375236e9137aca48d7186a77dc58c
EZ <ericzklm@hotmail.com>
2026-04-23 21:29:58 -0700

    Chesnaught Rerun Strats

--- src/uicomponents/StratLoadField.tsx
+++ src/uicomponents/StratLoadField.tsx
@@ -29,7 +29,7 @@ type StratOption = {
     credits?: string,
 }
 
-const SHOW_BOSSES_FIRST: SpeciesName[] = ["Samurott"] as SpeciesName[];
+const SHOW_BOSSES_FIRST: SpeciesName[] = ["Chesnaught"] as SpeciesName[];
 
 function stratDexEntryToOption(options: StratOption[], index: number, boss: string, stratname: string, stratpath: string) {
     import(`../data/strats/${stratpath}.json`)

33b676eaadf9b568c2c24295b405209a9412685f
EZ <ericzklm@hotmail.com>
2026-05-07 18:29:35 -0700

    Greninja Rerun Strats

--- src/uicomponents/StratLoadField.tsx
+++ src/uicomponents/StratLoadField.tsx
@@ -31,7 +31,7 @@ type StratOption = {
     credits?: string,
 }
 
-const SHOW_BOSSES_FIRST: SpeciesName[] = ["Delphox"] as SpeciesName[];
+const SHOW_BOSSES_FIRST: SpeciesName[] = ["Greninja"] as SpeciesName[];
 
 function stratDexEntryToOption(options: StratOption[], index: number, boss: string, stratname: string, stratpath: string) {
     import(`../data/strats/${stratpath}.json`)

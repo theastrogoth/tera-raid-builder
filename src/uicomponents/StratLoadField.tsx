f58bfdbd5de20a0da9c6d68c83ad055631575818
EZ <ericzklm@hotmail.com>
2026-06-18 20:55:16 -0700

    Inteleon Rerun Strats

--- src/uicomponents/StratLoadField.tsx
+++ src/uicomponents/StratLoadField.tsx
@@ -31,7 +31,7 @@ type StratOption = {
     credits?: string,
 }
 
-const SHOW_BOSSES_FIRST: SpeciesName[] = ["Cinderace"] as SpeciesName[];
+const SHOW_BOSSES_FIRST: SpeciesName[] = ["Inteleon"] as SpeciesName[];
 
 function stratDexEntryToOption(options: StratOption[], index: number, boss: string, stratname: string, stratpath: string) {
     import(`../data/strats/${stratpath}.json`)
